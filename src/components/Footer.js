07b8179a036da2cb95a9a426b9e47697300e9215
pinpointdev90 <kojkalosmi@gmail.com>
2022-11-23 09:35:41 +0900

    Update Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,7 +17,7 @@ function Footer() {
           <h3>Designed and Developed by Pin Point Dev</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} SB</h3>
+          <h3>Copyright © {year} PPD 90</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
