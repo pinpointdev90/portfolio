6af60ff3a8fbd10146adfb9127bbcd4ccf44719f
pinpointdev90 <kojkalosmi@gmail.com>
2022-11-23 09:14:25 +0900

    Update Image

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -47,10 +47,10 @@ function Projects() {
             <ProjectCard
               imgPath={editor}
               isBlog={false}
-              title="Editor.io"
+              title="Editor"
               description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/pinpointdev90/Editor.io"
-              demoLink="https://editor.pinpointdev.tech/"              
+              ghLink="https://github.com/pinpointdev90/editor"
+              demoLink="https://editor-pinpointdev90.netlify.app/"              
             />
           </Col>
 
