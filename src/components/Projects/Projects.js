2e687f8f4a236ab3ea77ec611172e1340dba886b
pinpointdev90 <kojkalosmi@gmail.com>
2022-11-23 09:33:38 +0900

    Update Demo Link

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -60,7 +60,7 @@ function Projects() {
               isBlog={false}
               title="Plant AI"
               description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/pinpointdev90/Plant_AI"
+              ghLink="https://github.com/pinpointdev90/plant-AI"
               demoLink="https://plant49-ai.herokuapp.com/"
             />
           </Col>
